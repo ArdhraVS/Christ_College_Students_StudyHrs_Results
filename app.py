88a932ec021d6ad0532e4a403671d7f7095a6de9
ArdhraVS <ardhra139@gmail.com>
2026-09-17 11:23:01 +0530

    Update app.py

--- app.py
+++ app.py
@@ -3,13 +3,13 @@ import joblib
 model = joblib.load("LogisticRegression_StudyHours_model.pkl")
 st.title("Student Pass/Fail based on Study Hours")
 hours = st.number_input("Enter Study Hours: ", min_value=0.0, max_value=15.0, value=5.0)
-
+attnd= st.number_input("Enter Attendance: ",min_value=0.0, max_value=100.0, value=75.0)
 if st.button("Predict"):
-    probabilities = model.predict_proba([[hours]])[0]
+    probabilities = model.predict_proba([[hours,attnd]])[0]
     fail_percentage = probabilities[0] * 100
     pass_percentage = probabilities[1] * 100
     
-    prediction = model.predict([[hours]])
+    prediction = model.predict([[hours,attnd]])
     if prediction[0] == 1:
         st.success("Pass")
         st.write(f"Pass Probability: {pass_percentage:.2f}%")
