6db9a5c0c02b5a90aef4e9643d2ddcf8dceacab7
ArdhraVS <ardhra139@gmail.com>
2026-09-17 10:40:15 +0530

    Update app.py

--- app.py
+++ app.py
@@ -11,6 +11,6 @@ if st.button("Predict"):
     
     prediction = model.predict([[hours]])
     if prediction[0] == 1:
-        st.success(f"Probability of Pass: {pass_percentage:.2f}%")
+        st.success(f"Pass Probability: {pass_percentage:.2f}%")
     else:
-        st.error(f"Probability of Fail: {fail_percentage:.2f}%")
+        st.error(f"Fail Probability: {fail_percentage:.2f}%")
